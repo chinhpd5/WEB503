39c74879bf5bd88f6e63c5847a9a548067923a9d
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-06-08 18:07:13 +0700

    uploadinary

--- src/controllers/common.controller.js
+++ src/controllers/common.controller.js
@@ -1,4 +1,5 @@
 import Upload from "../models/upload.model"
+import cloudinary from "../utils/cloudinary";
 
 export const upload = async (req,res) =>{
   try {
@@ -13,5 +14,27 @@ export const upload = async (req,res) =>{
     
   } catch (error) {
     return res.status(500).json({message: error.message})
+  }
+}
+
+export const uploadinary = async (req,res) =>{
+   try {
+    const fileStr = req.file.buffer.toString('base64');
+    const uploadedResponse = await cloudinary.uploader.upload(
+      `data:${req.file.mimetype};base64,${fileStr}`,
+      {
+        folder: 'web503.01' // tùy chọn, có thể bỏ
+      }
+    );
+
+    // lưu trữ trong db
+    await Upload.create({image: uploadedResponse.secure_url})
+    
+    return res.status(200).json({
+      message: 'Upload thành công!',
+      url: uploadedResponse.secure_url
+    });
+  } catch (err) {
+    return res.status(500).json({ error: 'Upload thất bại', details: err.message });
   }
 }
--- src/routers/common.router.js
+++ src/routers/common.router.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { upload } from '../controllers/common.controller';
-import { upload as uploadMiddleware} from '../middlewares/upload'
+import { upload, uploadinary } from '../controllers/common.controller';
+import { upload as uploadMiddleware} from '../middlewares/upload';
+import { upload as uploadinaryMiddleware} from '../middlewares/uploadinary'
 
 const router = express.Router();
 
 router.post('/upload',uploadMiddleware.single("image"),upload)
-
+router.post('/upload-cloudinary',uploadinaryMiddleware.single("image"),uploadinary)
 
 export default router;
